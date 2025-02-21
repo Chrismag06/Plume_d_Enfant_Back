a7c8c8f03ff48e33d039c9b726e5ac1344be0845
chrismag06 <chrismag06000@gmail.com>
2025-02-21 14:56:20 +0100

    lecture audio histoire OK

--- src/main/java/com/poec/plumedenfant/controller/TtsController.java
+++ src/main/java/com/poec/plumedenfant/controller/TtsController.java
@@ -1,13 +1,18 @@
 package com.poec.plumedenfant.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.poec.plumedenfant.dao.model.Histoire;
+import com.poec.plumedenfant.service.HistoireService;
 import com.poec.plumedenfant.tts.GoogleTextToSpeechService;
 import com.poec.plumedenfant.tts.LanguageCode;
 import com.poec.plumedenfant.tts.TextToSpeechService;
@@ -19,8 +24,11 @@ import com.poec.plumedenfant.tts.VoiceGender;
 @RequestMapping("/api/tts")
 public class TtsController {
 
-    @GetMapping(value = "/speak", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<byte[]> speak(@RequestParam String text) {
+    @Autowired
+	private HistoireService histoireService;
+
+    @GetMapping(value = "/speak/{idHistoire}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> speak(@PathVariable int idHistoire) {
                
             LanguageCode languageCode =  LanguageCode.fr_FR;
             //VoiceGender gender = VoiceGender.FEMALE;
@@ -30,16 +38,36 @@ public class TtsController {
 
             TextToSpeechService ttsService = new GoogleTextToSpeechService();
 
+            System.out.println("id histoire:" + idHistoire);
+
             try {
-                audioData = ttsService.convertTextToSpeech(text, languageCode, gender, encoding, 1.0).toByteArray();
+
+                Optional<Histoire> histoireRecup = histoireService.getHistoireById(idHistoire);
+                if(histoireRecup.isPresent()) {
+                    System.out.println("Creation audio");
+                    audioData = ttsService.convertTextToSpeech(histoireRecup.get().getCorps(), languageCode, gender, encoding, 1.0).toByteArray();
+                    if (audioData == null) {
+                        System.out.println("Audio data null...");
+                        return ResponseEntity.internalServerError().build(); 
+                    }
+                } else {
+                    System.out.println("Histoire not found");
+                    return ResponseEntity.notFound().build(); 
+                }
+
+                
             } catch (Exception e) {
                 e.printStackTrace();
+                return ResponseEntity.internalServerError().build();
             }
 
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"speech.wav\"")
-                     .body(audioData);
-
+            if (audioData != null) { // Vérification avant de retourner la réponse
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"speech.wav\"")
+                        .body(audioData);
+            } else {
+                return ResponseEntity.internalServerError().build(); // Gestion du cas audioData null
+            }
 
     }
 }
